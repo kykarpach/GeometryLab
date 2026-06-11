8449c6b5759c6a0d0859a91a2b9f1c4c467fb964
kykarpach <zirnovakov52@gmail.com>
2026-06-11 13:15:15 +0700

    Поправил деление на ноль и обработал некорректные индексы

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,142 +1,153 @@
-#include "C:\Users\777\Desktop\Proga\C++\GeometryLab\include\geometry\Vec2.h"
+#include "geometry/Vec2.h"
 #include <iostream>
+#include <cmath>
+#include <stdexcept>
+
 
 namespace geom {
 
+Vec2::Vec2(): x_(0), y_(0) {}
+Vec2::Vec2(double x, double y): x_(x), y_(y) {}
 
-    Vec2(): x_(0), y_(0) {}
-    Vec2(double x, double y): x_(x), y_(y) {}
+double Vec2::x() const {
+    return x_;
+}
 
-    double x()const{
+double Vec2::y() const {
+    return y_;
+}
+
+double Vec2::length() const {
+    return std::sqrt(x_ * x_ + y_ * y_);
+}
+
+double Vec2::length2() const {
+    return x_ * x_ + y_ * y_;
+}
+
+Vec2 Vec2::normalized() const {
+    if(x_ != 0 && y_ != 0){
+        Vec2 answ(x_ / length(), y_ / length());
+        return answ;
+    }
+    std::invalid_argument("vector has zero length");
+    
+}
+
+double Vec2::dot(const Vec2& other) const {
+    return x_ * (other.x_) + y_ * (other.y_);
+}
+
+double Vec2::cross(const Vec2& other) const {
+    return x_ * (other.y_) - y_ * (other.x_);
+}
+
+
+const double& Vec2::operator[](std::size_t index) const {
+    if (index == 0) {
         return x_;
     }
-    double y()const{
+    if (index == 1) {
         return y_;
     }
-
-    double length() const{
-        return sqrt(x_*x_ + y_*y_);
-    }
-    double length2() const{
-        return x_*x_ + y_*y_;
-    }
-
-    Vec2 normalized() const{
-        Vec2 answ (x_/length(), y_/length()); // дописать проверку что длинна не ноль
-        return answ;
+    throw std::out_of_range("Vec2 index out of range");
+}
+double& Vec2::operator[](std::size_t index) {
+    if (index == 0) {
+        return x_;
     }
 
-    double dot(const Vec2& other) const{
-        return x_ * (other.x_) + y_ * (other.y_) ;
+    if (index == 1) {
+        return y_;
     }
+    
+    throw std::out_of_range("Vec2 index out of range");
+}
 
-    double cross(const Vec2& other) const{
-        return x_*(other.y_) - y_*(other.x_);
-    }
+// Унарные + и -
+Vec2 Vec2::operator+() const {
+    Vec2 answ = *this;
+    return answ;
+}
+Vec2 Vec2::operator-() const {
+    Vec2 answ (-x_, -y_);
+    return answ;
+}
 
-    // потом реализовать поведение в слуяае некорректного идекса
-    const double& operator []  (std::size_t index) const{
-  
-        if(index == 0){
-            return x_;
-        }
-        if(index == 1){
-            return y_;
-        }
-    }
-     // потом реализовать поведение в слуяае некорректного идекса
-    double& operator [] (std::size_t index) {
+Vec2& Vec2::operator+=(const Vec2& other) {
+    x_ += other.x();
+    y_ += other.y();
+    return *this;
+}
+Vec2& Vec2::operator-=(const Vec2& other) {
+    x_ -= other.x();
+    y_ -= other.y();
+    return *this;
+}
 
-        if(index == 0){
-            return x_;
-        }
-        if(index == 1){
-            return y_;
-        }
-    }
-
-    //  Унарныые +  b -
-    Vec2 operator+() const{
-        Vec2 answ = *this; //Через конструктор копирования а не через конструктор
-        return answ;
-    }
-    Vec2 operator-() const{
-        Vec2 answ = -(*this);//Через конструктор копирования а не через конструктор
-        return answ;
-    }
-    //----------------------------
-
-    Vec2& operator+=(const Vec2& other){
-        x_ += other.x();
-        y_ += other.y();
+Vec2& Vec2::operator*=(double k) {
+    x_ *= k;
+    y_ *= k;
+    return *this;
+}
+Vec2& Vec2::operator/=(double k) {
+    if (k != 0){
+        x_ /= k;
+        y_ /= k;
         return *this;
     }
-    Vec2& operator-=(const Vec2& other){
-        x_ -= other.x();
-        y_ -= other.y();
-        return *this;
-    }
+    std::invalid_argument("you can't divide by zero");
+}
 
-    Vec2& operator*=(double k){
-        x_*=k;
-        y_*=k;
-        return *this;
-    }
-    Vec2& operator/=(double k){
-        x_/=k;
-        y_/=k;
-        return *this;
-    }
+// Бинарные
+Vec2 operator+(const Vec2& v1, const Vec2& v2) {
+    Vec2 answ(v1.x() + v2.x(), v1.y() + v2.y());
+    return answ;
+}
+Vec2 operator-(const Vec2& v1, const Vec2& v2) {
+    Vec2 answ(v1.x() - v2.x(), v1.y() - v2.y());
+    return answ;
+}
 
+Vec2 operator*(const Vec2& v, double k) {
+    Vec2 answ(v.x() * k, v.y() * k);
+    return answ;
+}
+Vec2 operator*(double k, const Vec2& v) {
+    Vec2 answ(v.x() * k, v.y() * k);
+    return answ;
+}
 
-
-    // Бинарные------------------
-    Vec2 operator + (const Vec2& v1, const Vec2& v2){
-        Vec2 answ(v1.x() + v2.x(), v1.y() + v2.y());
+Vec2 operator/(const Vec2& v, double k){
+    if(k != 0){
+        Vec2 answ(v.x() / k, v.y() / k);
         return answ;
     }
-    Vec2 operator - (const Vec2& v1, const Vec2& v2){
-        Vec2 answ(v1.x() - v2.x(), v1.y() - v2.y());
+    std::invalid_argument("you can't divide by zero");
+}
+Vec2 operator/(double k, const Vec2& v) {
+    if(k != 0){
+        Vec2 answ(v.x() / k, v.y() / k);
         return answ;
     }
+    std::invalid_argument("you can't divide by zero");
+}
 
-    Vec2 operator*(const Vec2& v, double k){
-        Vec2 answ (v.x()*k, v.y()*k);
-        return answ;
-    }
-    Vec2 operator*(double k, const Vec2& v){
-        Vec2 answ (v.x()*k, v.y()*k);
-        return answ;
-    }
+// Операторы ввода и вывода
+std::istream& operator>>(std::istream& in, Vec2& v) {
+    double x, y;
+    char dummy;
 
-    Vec2 operator/(const Vec2& v, double k){
-        Vec2 answ (v.x()/k, v.y()/k);
-        return answ;
-    }
-    Vec2 operator/(double k, const Vec2& v){
-        Vec2 answ (v.x()/k, v.y()/k);
-        return answ;
-    }
-    //----------------------------
+    in >> x >> dummy;
+    in >> y >> dummy;
 
-    //Операторы ВВОДА и ВЫВОДА
+    v = Vec2(x, y);
 
-    std::istream& operator >> (std::istream& in, Vec2& v){
-        double x, y;
-        char dummy;
+    return in;
+}
+std::ostream& operator<<(std::ostream& out, const Vec2& v) {
+    out << "coordinate: (" << v.x() << ", " << v.y() << ") \n";
+    return out;
+}
 
-        in >> x >> dummy;
-        in >> y >> dummy;
-
-        v = Vec2(x, y);
-
-        return in;
-    };
-    std::ostream& operator << (std::ostream& out, const Vec2& v){
-
-        out << "coordinate: (" << v.x() << ", " << v.y() << ") \n"; 
-
-        return out;
-    };
-
-};
+} // namespace geom
